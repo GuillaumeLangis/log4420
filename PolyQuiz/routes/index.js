a99706234b6c2f646970ed7acbed44258fd56c42
Guillaume Langis <langis.guillaume@gmail.com>
2016-09-28 16:19:17 -0400

    des commit

--- PolyQuiz/routes/index.js
+++ PolyQuiz/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Acceuil' });
+});
+
+router.get('/instructions', function(req, res, next) {
+	res.render('instructions', {title: 'Instructions'});
+});
+
+router.get('/mypage', function(req, res, next) {
+	res.render('mypage', {title: 'Tableau de Bord'});
 });
 
 module.exports = router;
