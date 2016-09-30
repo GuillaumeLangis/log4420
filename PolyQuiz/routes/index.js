ca2bae7ba6da4240c41ba6a86d2ece39398eb43a
Guillaume Langis <langis.guillaume@gmail.com>
2016-09-30 16:20:43 -0400

    Ajouts des questions Tableau de bord complet

--- PolyQuiz/routes/index.js
+++ PolyQuiz/routes/index.js
@@ -14,4 +14,22 @@ router.get('/mypage', function(req, res, next) {
 	res.render('mypage', {title: 'Tableau de Bord'});
 });
 
+router.get('/quicktest', function(req, res, next) {
+	res.render('quicktest', {title: 'Question Rapide #1'});
+});
+router.get('/quicktest2', function(req, res, next) {
+	res.render('quicktest2', {title: 'Question Rapide #2'});
+});
+
+router.get('/test', function(req, res, next) {
+	res.render('test', {title: 'Question #1 sur 2'});
+});
+router.get('/test2', function(req, res, next) {
+	res.render('test2', {title: 'Question #2 sur 2'});
+});
+router.get('/results', function(req, res, next) {
+	res.render('results', {title: 'Resultats'});
+});
+
+
 module.exports = router;
